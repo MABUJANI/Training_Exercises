43e617f14e748ce6f2512dff82d43012c1918ce4
MABUJANI <33048350+MABUJANI@users.noreply.github.com>
2020-02-06 00:37:59 +0530

    Producer Consumer Problem with 2 producers and 1 consumer

---
+++ Assessment1/ProducerConsumer.java
@@ -0,0 +1,93 @@
+import java.util.LinkedList;
+
+public class ExampleThread {
+	public static void main(String args[]) throws InterruptedException
+	{
+		final PC pc = new PC();
+		
+		Thread t1 = new Thread(new Runnable(){
+			
+			public void run()
+			{
+				try{
+					pc.produce();
+				}
+				catch(InterruptedException e){
+					e.printStackTrace();
+					
+				}
+			}
+			
+		});
+		Thread t2 = new Thread(new Runnable(){
+			public void run()
+			{
+				try{
+					pc.produce();
+				}
+				catch(InterruptedException e){
+					e.printStackTrace();
+				}
+			}
+		});
+		
+		
+		Thread t3 = new Thread(new Runnable(){
+			public void run(){
+				try{
+					pc.consume();
+				}
+				catch(InterruptedException e){
+					e.printStackTrace();
+				}
+			}
+			
+		});
+		
+		
+		t1.start();
+		t2.start();
+		t3.start();
+		
+		t1.join();
+		t2.join();
+		t3.join();
+	}
+	
+	public static class PC{
+		
+		LinkedList<Integer> list = new LinkedList();
+		int size = 3;
+		
+		public void produce() throws InterruptedException{
+			int value = 0;
+			while(true){
+				synchronized(this){
+					while(list.size() == size)
+						wait();
+					System.out.println("Producer produced -> "+ value + Thread.currentThread().getName());
+					list.add(value++);
+					notify();
+					Thread.sleep(2000);
+					
+				}
+			}
+		}
+		
+		public void consume() throws InterruptedException{
+			while(true){
+				synchronized(this){
+					while(list.size() == 0)
+						wait();
+					
+					int val = list.removeFirst();
+					System.out.println("Consumer consumed -> "+ val + Thread.currentThread().getName());
+					notify();
+					Thread.sleep(2000);
+				}
+			}
+		}
+		
+	}
+
+}
